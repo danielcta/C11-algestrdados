787001e4784678b8c758c0c5f91b43ffb40482aa
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-30 11:44:25 -0300

    Add files via upload

---
+++ C11EX07.java
@@ -0,0 +1,47 @@
+import javax.swing.JOptionPane;
+
+public class C11EX07 
+{
+    public static void main(String[] args) 
+    {
+        double temperatura, maiorTemperatura = Double.MIN_VALUE, menorTemperatura = Double.MAX_VALUE, media = 0, diasMuitoQuentes = 0, dia;
+    
+        for (dia = 1; dia <= 121; dia++)
+        {
+            temperatura = Double.parseDouble(JOptionPane.showInputDialog(null,
+            "Informe a temperatura do dia " + (int) dia,
+            "Exercício 07",
+            JOptionPane.QUESTION_MESSAGE));
+
+            while (temperatura < 485)
+            {
+                media += temperatura;
+                System.out.println("batata");
+
+                temperatura = Double.parseDouble(JOptionPane.showInputDialog(null,
+                "Informe a temperatura do dia " + (int) dia,
+                "Exercício 07",
+                JOptionPane.QUESTION_MESSAGE));
+
+                if (temperatura >= 485) 
+                {
+                    break
+                    System.out.println("CÓDIGO FINALIZADO!");
+                } else if (temperatura > maiorTemperatura) 
+                {
+                    maiorTemperatura = temperatura;
+                }
+
+                if (temperatura <= menorTemperatura) 
+                {
+                    menorTemperatura = temperatura;    
+                }
+
+                System.out.printf("%.2f %.2f %.2f ");
+            }
+        }
+
+    }
+
+
+}
